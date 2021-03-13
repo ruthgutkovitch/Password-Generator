faca64a8494d01e99692b31d1254b9f440fd5bc9
ruthgutkovitch <75093922+ruthgutkovitch@users.noreply.github.com>
2021-03-13 22:05:02 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import string
 import random
+from PyDictionary import PyDictionary
 
 def getPasswordLen():
     length = int(input("How long do you want your password: "))
@@ -48,8 +49,7 @@ def generatePassword(choises_list, length):
         return str
 
 
-if __name__ == '__main__':
-    print("Welcome to Password Generator!!!")
+def generateRandomPassword():
     length = getPasswordLen()
     list = []
     list.append(useDigits())
@@ -58,5 +58,61 @@ if __name__ == '__main__':
     list.append(usePunctuation())
     list.append(repeatCharacters())
     password = generatePassword(list, length)
-    print (password)
+    return password
+
+def shuffleSentence():
+
+    sentence = input("Please Enter Your Sentence to Be Shuffled: ")
+    l1 = list(sentence)
+    random.shuffle(l1)
+    result = ''.join(l1)
+    return result
+
+def enterSentence():
+    sentence = input("Please Enter Your Sentence For Creating Password: ")
+    l1 = sentence.split(' ')
+    random.shuffle(l1)
+    result = ''
+    for i in range(len(l1)):
+        letter = l1[i][0]
+        result += letter
+
+    return result
+
+def dictionaryMethod():
+
+    dictionary = PyDictionary()
+    sentence = input("Please Enter Random Word: ")
+    meaning = dictionary.meaning(sentence)
+    value = str(meaning.values())
+    l1 = value.split(' ')
+    num = int(input(" How Many Words Do You Want to Use? "))
+    l2 = []
+    for i in range(num):
+        word = random.choice(l1)
+        l2.append(word)
+    random.shuffle(l2)
+    result = ''
+    for i in range(len(l2)):
+        letter = l2[i][0]
+        result += letter
+
+    return result
+
+if __name__ == '__main__':
+    print("Welcome to Password Generator!!!")
+    print ("Option 1:Generate Random Password \nOption 2 : Enter Sentence to Shuffle \n" +
+           "Option 3: Enter Sentence For Creating Password \n "+
+           "Option 4: Dictionary Method For Creating Password ")
+    option = int(input(" Choose one of the options: "))
+    password = ''
+    if option == 1:
+        password = generateRandomPassword()
+    if option == 2:
+        password = shuffleSentence()
+    if option == 3:
+        password = enterSentence()
+    if option == 4:
+        password = dictionaryMethod()
+    print ("Your Password is : ",password)
 
